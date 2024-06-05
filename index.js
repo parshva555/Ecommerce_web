b6efa06b11073a46ac09eb76f555b5344e0ff0a3
parshva555 <parshvadani@gmail.com>
2024-06-06 02:36:58 +0530

    Top sale logic

--- index.js
+++ index.js
@@ -3,5 +3,20 @@ $(document).ready(function(){
         dots: true,
         items: 1
     });
-
+    $("#top-sale .owl-carousel").owlCarousel({
+        loop: true,
+        nav: true,
+        dots: false,
+        responsive : {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000 : {
+                items: 5
+            }
+        }
+    });
 })
