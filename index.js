f9ee1d51b3df5f99f6c42e119728b646a3e4230f
parshva555 <parshvadani@gmail.com>
2024-06-06 13:00:58 +0530

    increasing and decreasing quantity in product page

--- index.js
+++ index.js
@@ -59,5 +59,27 @@ $(document).ready(function(){
                 }
             }
         })
+        let $qty_up = $(".qty .qty-up");
+        let $qty_down = $(".qty .qty-down");
+        // let $input = $(".qty .qty_input");
+
+        $qty_up.click(function(e){
+            let $input = $(`.qty_input[data-id='${$(this).data("id")}']`);
+            if($input.val() >= 1 && $input.val() <= 9){
+                $input.val(function(i, oldval){
+                    return ++oldval;
+                });
+            }
+        });
+           $qty_down.click(function(e){
+            let $input = $(`.qty_input[data-id='${$(this).data("id")}']`);
+            if($input.val() > 1 && $input.val() <= 10){
+                $input.val(function(i, oldval){
+                    return --oldval;
+                });
+            }
+        });
+    
+    
 
 })
