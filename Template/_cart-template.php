b75c9c84273bb07008ff9fe93a4ddd979947bea7
parshva555 <parshvadani@gmail.com>
2024-06-09 18:04:54 +0530

    Display cart on cart page

--- Template/_cart-template.php
+++ Template/_cart-template.php
@@ -12,16 +12,16 @@
                     <?php
                         foreach($product->getData('cart') as $item) :
                             $cart = $product->getProduct($item['item_id']);
-                            print_r($cart);
+                            array_map(function($item){
                     ?>
                     <!-- cart item -->
                     <div class="row border-top py-3 mt-3">
                         <div class="col-sm-2">
-                            <img src="./assets/products/1.png" style="height: 120px;" alt="cart1" class="img-fluid">
+                            <img src="<?php echo $item['item_image'] ?? "./assets/products/1.png" ?>" style="height: 120px;" alt="cart1" class="img-fluid">
                         </div>
                         <div class="col-sm-8">
-                            <h5 class="font-mukta font-size-20">Samsung Galaxy 10</h5>
-                            <small>by Samsung</small>
+                            <h5 class="font-mukta font-size-20"><?php echo $item['item_name'] ?? "Unknown"; ?></h5>
+                            <small><?php echo $item['item_brand'] ?? "Brand"; ?></small>
                             <!-- product rating -->
                             <div class="d-flex">
                                 <div class="rating text-warning font-size-12">
@@ -51,12 +51,13 @@
 
                         <div class="col-sm-2 text-right">
                             <div class="font-size-20 text-danger font-mukta">
-                                $<span class="product_price">152</span>
+                                $<span class="product_price" data-id="<?php echo $item['item_id'] ?? '0'; ?>"><?php echo $item['item_price'] ?? 0; ?></span>
                             </div>
                         </div>
                     </div>
                     <!-- !cart item -->
                     <?php
+                            }, $cart); // closing array map function
                         endforeach;
                     ?>
                 </div>
