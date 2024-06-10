fb2164c5d5a0943f8d6c2e68c1b11c4e8634afd5
parshva555 <parshvadani@gmail.com>
2024-06-10 16:47:34 +0530

    Changing the button based on whethe item is in the cart or not

--- Template/_special-price.php
+++ Template/_special-price.php
@@ -1,21 +1,25 @@
- <!-- Special Price -->
+<!-- Special Price -->
 <?php
-    $brand = array_map(function ($pro){ return $pro['item_brand']; }, $product_shuffle);
-    $unique = array_unique($brand);
-    sort($unique);
-    shuffle($product_shuffle);
+$brand = array_map(function ($pro){ return $pro['item_brand']; }, $product_shuffle);
+$unique = array_unique($brand);
+sort($unique);
+shuffle($product_shuffle);
+
+// request method post
 if($_SERVER['REQUEST_METHOD'] == "POST"){
     if (isset($_POST['special_price_submit'])){
         // call method addToCart
         $Cart->addToCart($_POST['user_id'], $_POST['item_id']);
     }
 }
-    ?>
 
+$in_cart = $Cart->getCartId($product->getData('cart'));
+
+?>
 <section id="special-price">
     <div class="container">
         <h4 class="font-rubik font-size-20">Special Price</h4>
-        <div id="filters" class="button-group text-right font-mukta font-size-16">
+        <div id="filters" class="button-group text-right font-baloo font-size-16">
             <button class="btn is-checked" data-filter="*">All Brand</button>
             <?php
             array_map(function ($brand){
@@ -25,7 +29,7 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
         </div>
 
         <div class="grid">
-            <?php array_map(function ($item){ ?>
+            <?php array_map(function ($item) use($in_cart){ ?>
                 <div class="grid-item border <?php echo $item['item_brand'] ?? "Brand" ; ?>">
                     <div class="item py-2" style="width: 200px;">
                         <div class="product font-rale">
@@ -43,9 +47,15 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
                                     <span>$<?php echo $item['item_price'] ?? 0 ?></span>
                                 </div>
                                 <form method="post">
-                                    <input type="hidden" name="item_id" value="<?php echo $item['item_id'] ?? "1" ?>">
-                                    <input type="hidden" name="user_id" value="<?php echo 1?>">
-                                    <button type="submit" name="special_price_submit" class="btn btn-warning font-size-12">Add to Cart</button>
+                                    <input type="hidden" name="item_id" value="<?php echo $item['item_id'] ?? '1'; ?>">
+                                    <input type="hidden" name="user_id" value="<?php echo 1; ?>">
+                                    <?php
+                                    if (in_array($item['item_id'], $in_cart ?? [])){
+                                        echo '<button type="submit" disabled class="btn btn-success font-size-12">In the Cart</button>';
+                                    }else{
+                                        echo '<button type="submit" name="top_sale_submit" class="btn btn-warning font-size-12">Add to Cart</button>';
+                                    }
+                                    ?>
                                 </form>
                             </div>
                         </div>
